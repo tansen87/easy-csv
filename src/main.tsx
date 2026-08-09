1bcb039e4149134a067bd0c41731d847c1bda4b3
tansen <tansen87@qq.com>
2026-08-09 12:36:07 +0800

    fix: bundle fontsource fonts correctly in production build

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@fontsource-variable/inter/index.css";
+import "@fontsource/jetbrains-mono/index.css";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "@/components/setting/ThemeProvider.tsx";
